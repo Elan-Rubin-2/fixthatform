a0e6e88dd3325a5418d09941548b963dc5137318
nwumefr <chibuikenwume@gmail.com>
2024-09-22 05:04:11 -0400

    adde functionality to flask app to collect user info to allow chatbot to use name age and gender as a ref

--- static/chatbotUtil.js
+++ static/chatbotUtil.js
@@ -21,3 +21,32 @@ function sendBotRes() {
         });
 }
 
+function sendUserInfo() {
+    const name = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const sex = document.getElementById('sex').value;
+    
+    const data = {
+        name: name,
+        age: age,
+        sex: sex,
+    };
+
+    fetch('/get_user_info', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(result => {
+            console.log(result)
+            //document.getElementById('chatResponse').textContent = result.response;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+
